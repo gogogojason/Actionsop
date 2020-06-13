6177b751f3651f3a1a53e3ee000a445442b16054
gogogojason <65128346+gogogojason@users.noreply.github.com>
2020-06-13 15:46:57 +0800

    Update x86public.sh

--- x86public.sh
+++ x86public.sh
@@ -1,10 +1,10 @@
 # cd $WORK_PATH 目录下,先运行的 public.h -> 设备.h -> scripts/feeds install -a
 # 定义部分以及需要添加对应APP必须的文件
 device_name='JasonRouter'                                                      # 自定义设备名
-wifi_name="ZSNet"                                                          # 自定义Wifi 名字
-lan_ip='192.168.5.1'                                                        # 自定义Lan Ip地址
+wifi_name="RMWiFi"                                                          # 自定义Wifi 名字
+lan_ip='192.168.2.1'                                                        # 自定义Lan Ip地址
 utc_name='Asia\/Shanghai'                                                   # 自定义时区
-ver_name='D200517'                                                          # 版本号
+ver_name='D200613'                                                          # 版本号
 delete_bootstrap=false                                                      # 是否删除默认主题 true 、false
 default_theme='luci-theme-argon_new'                                        # 默认主题 结合主题文件夹名字
 theme_argon='https://github.com/sypopo/luci-theme-argon-mc.git'             # 主题地址
@@ -13,8 +13,6 @@ adguardhome_url='https://github.com/rufengsuixing/luci-app-adguardhome.git' # ad
 lienol_url='https://github.com/Lienol/openwrt-package.git'                  # Lienol 包地址
 vssr_url_rely='https://github.com/jerrykuku/lua-maxminddb.git'              # vssr lua-maxminddb依赖
 vssr_url='https://github.com/jerrykuku/luci-app-vssr.git'                   # vssr地址
-vssr_plus_rely='https://github.com/Leo-Jo-My/my.git'                        # vssr_plus 依赖
-vssr_plus='https://github.com/Leo-Jo-My/luci-app-vssr-plus.git'             # vssr_plus 地址
 filter_url='https://github.com/destan19/OpenAppFilter.git'                  # AppFilter 地址
 smartdns_url='https://github.com/pymumu/openwrt-smartdns'                   # SmartDNS运行程序
 smartdnsapp_url='https://github.com/pymumu/luci-app-smartdns.git'           # SmartDNS-App
@@ -31,6 +29,9 @@ sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.
 echo "设置lan ip"
 sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 
+echo "修改argon主题背景色"
+sed -i "s/#5e72e4/#00C000/g" feeds/otherpackges/luci-theme-argon_new/luasrc/view/themes/argon/header.htm
+
 echo "修改时区"
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
 
@@ -41,12 +42,11 @@ sed -i "s/bootstrap/argon/g" feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 echo "修改版本信息"
-sed -i "s/R20.5.9/R20.5.9\/hfy166 Ver.$ver_name/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/R20.5.20/R20.5.20\/hfy166 Ver.$ver_name/g" package/lean/default-settings/files/zzz-default-settings
 
 #echo '添加serverchan'
 #git clone $serverchan_url package/luci-app-serverchan
 #echo 'CONFIG_PACKAGE_luci-app-serverchan=y' >> .config
-
 
 #echo '添加主题argon'
 #git clone $theme_argon package/lean/luci-theme-argon-mc
@@ -86,11 +86,11 @@ git clone $adguardhome_url package/lean/luci-app-adguardhome
 #echo 'CONFIG_PACKAGE_luci-app-filebrowser=y' >> .config
 #echo 'CONFIG_PACKAGE_luci-i18n-filebrowser-zh-cn=y'  >> .config
 
-# echo '添加HelloWord,并使用包默认的配置'  # TODO 这个的配置文件和SSP 冲突
-# git clone $vssr_url_rely package/lean/lua-maxminddb
-# git clone $vssr_url package/lean/luci-app-vssr
-# echo 'CONFIG_PACKAGE_luci-app-vssr=y' >> .config
-# echo 'CONFIG_PACKAGE_luci-i18n-vssr-zh-cn=y'  >> .config
+echo '添加HelloWord,并使用包默认的配置'  # TODO 这个的配置文件和SSP 冲突
+git clone $vssr_url_rely package/lean/lua-maxminddb
+git clone $vssr_url package/lean/luci-app-vssr
+echo 'CONFIG_PACKAGE_luci-app-vssr=y' >> .config
+echo 'CONFIG_PACKAGE_luci-i18n-vssr-zh-cn=y'  >> .config
 
 #echo '添加Lienol包'
 #git clone $lienol_url package/Lienol
@@ -102,8 +102,8 @@ git clone $adguardhome_url package/lean/luci-app-adguardhome
 #echo 'CONFIG_PACKAGE_appfilter=y' >> .config
 #echo 'CONFIG_PACKAGE_luci-i18n-oaf-zh-cn=y'  >> .config
 
-# echo '添加Leo-Jo-My的Hello World,并且使用默认包配置'
-# git clone $vssr_plus_rely package/lean/luci-vssr-plus-rely
-# git clone $vssr_plus_rely package/lean/luci-app-vssr-plus
-# echo 'CONFIG_PACKAGE_luci-app-vssr-plus=y' >> .config
-# echo 'CONFIG_PACKAGE_luci-i18n-vssr-plus-zh-cn=y'  >> .config
+ #echo '添加Leo-Jo-My的Hello World,并且使用默认包配置'
+ #git clone $vssr_plus_rely package/diy
+ #git clone $vvssr_plus package/luci-app-vssr-plus
+ #echo 'CONFIG_PACKAGE_luci-app-vssr-plus=y' >> .config
+ #echo 'CONFIG_PACKAGE_luci-i18n-vssr-plus-zh-cn=y'  >> .config
